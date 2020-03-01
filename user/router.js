47b8a7190094294f6d3d6e184abbf0f975245f28
Nolan Picini <nolanpic@gmail.com>
2020-02-29 22:51:13 -0800

    Add tests for handling additional responses from register route

--- user/router.js
+++ user/router.js
@@ -1,7 +1,27 @@
 const router = require('express').Router();
+const Users = require('./model');
 
-router.post('/register', (req, res) => {
-    res.status(201).send();
+router.post('/register', async (req, res) => {
+    const user = req.body;
+    if(!user.first_name
+        || !user.last_name
+        || !user.email
+        || !user.password) {
+            // *!* Need some better validation ^^^
+            res.status(400).json({ error: 'First & last name, email, and password are required' });
+            return;
+        }
+
+    // ensure email is not taken
+    const userWithEmail = await Users.getByEmail(user.email);
+
+    if(userWithEmail && userWithEmail.id) {
+        // user already exists with this email
+        res.status(409).json({ error: 'This email is already in use' });
+        return;
+    }
+
+    res.status(201).json({ token: '' });
 });
 
 module.exports = router;
--- user/router.test.js
+++ user/router.test.js
@@ -1,5 +1,7 @@
 const request = require('supertest');
 const server = require('../api/server');
+const Users = require('./model');
+const db = require('../data/db');
 
 describe('/users routes', () => {
 
@@ -12,11 +14,46 @@ describe('/users routes', () => {
             password: 'password123'
         };
 
+        beforeEach(async () => {
+            await db('users').truncate();
+        });
+
+        test('responds with 400 when body is invalid', async () => {
+            const invalidUser = {
+                email: '',
+                password: ''
+            };
+            const res = await request(server)
+                .post('/api/users/register')
+                .send(invalidUser);
+            expect(res.status).toBe(400);
+        });
+
+        test('responds with 409 when email is in use', async () => {
+            await Users.create(user);
+            const newUser = { ...user };
+
+            const res = await request(server)
+                .post('/api/users/register')
+                .send(newUser);
+
+            expect(res.status).toBe(409);
+        });
+
         test('responds with 201', async () => {
             const res = await request(server)
                 .post('/api/users/register')
                 .send(user);
             expect(res.status).toBe(201);
+        });
+
+        test('responds with token object', async () => {
+            const res = await request(server)
+                .post('/api/users/register')
+                .send(user);
+            expect(res.type).toMatch(/json/i);
+            expect(res.body.token).toBeDefined();
+            expect(res.body.token.length > 0).toBe(true);
         });
 
     });
