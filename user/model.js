00fb2cff27482d04bd04e0bb452ff8ac25cd603e
Nolan Picini <nolanpic@gmail.com>
2020-02-29 20:46:43 -0800

    Rough in user model

---
+++ user/model.js
@@ -0,0 +1,34 @@
+const db = require('../data/db');
+
+const get = () => {
+    
+};
+
+const getById = () => {
+
+};
+
+const getByEmail = () => {
+
+};
+
+const create = user => {
+
+};
+
+const update = (id, updates) => {
+
+};
+
+const remove = id => {
+
+};
+
+module.exports = {
+    get,
+    getById,
+    getByEmail,
+    create,
+    update,
+    remove
+};
