fc4fdc1de6578ff232224be21e379ca449873fee
Nolan Picini <nolanpic@gmail.com>
2020-03-02 23:17:54 -0800

    Add todo-item model w/ tests

--- todo-item/model.js
+++ todo-item/model.js
@@ -1,23 +1,33 @@
 const db = require('../data/db');
 
 const getByList = todoListId => {
-
+    return db('todo_items')
+        .where('todo_list_id', todoListId);
 };
 
 const getById = id => {
-
+    return db('todo_items')
+        .where({ id })
+        .first();
 };
 
-const create = (todoListId, todoItem) => {
-
+const create = async todoItem => {
+    const [id] = await db('todo_items')
+        .insert(todoItem, 'id');
+    return getById(id);
 };
 
-const update = (id, updates) => {
-
+const update = async (id, updates) => {
+    await db('todo_items')
+        .where({ id })
+        .update(updates);
+    return getById(id);
 };
 
 const remove = id => {
-
+    return db('todo_items')
+        .where({ id })
+        .delete();
 };
 
 module.exports = {
