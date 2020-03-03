52200a861b4f53661f0e90073ff61daa02a0f187
Nolan Picini <nolanpic@gmail.com>
2020-03-02 21:12:12 -0800

    Add todo-list model w/ tests

--- todo-list/model.js
+++ todo-list/model.js
@@ -1,23 +1,42 @@
 const db = require('../data/db');
 
 const getByUser = userId => {
-
+    return db('todo_lists as l')
+        .innerJoin('user_todo_lists as u', 'l.id', 'u.todo_list_id')
+        .where('u.user_id', userId);
 };
 
 const getById = id => {
-
+    return db('todo_lists')
+        .where({ id })
+        .first();
 };
 
-const create = (userId, list) => {
+const create = async (userId, list) => {
+    const [id] = await db('todo_lists').insert(list, 'id');
 
+    const todoListUser = {
+        user_id: userId,
+        todo_list_id: id,
+        owner: true
+    };
+
+    await db('user_todo_lists').insert(todoListUser);
+
+    return getById(id);
 };
 
-const update = (id, updates) => {
-
+const update = async (id, updates) => {
+    await db('todo_lists')
+        .where({ id })
+        .update(updates);
+    return getById(id);
 };
 
 const remove = id => {
-
+    return db('todo_lists')
+        .where({ id })
+        .del();
 };
 
 const addUserToList = (userId, id) => {
