d77fb4da8e9700bd12e8e5719d9ce288794e1a98
Nolan Picini <nolanpic@gmail.com>
2020-03-03 15:55:55 -0800

    Rough in route for getting todos

--- todo-item/model.js
+++ todo-item/model.js
@@ -7,18 +7,29 @@ const getByList = todoListId => {
 
 const getByQuery = (userId, query) => {
     const { todo_list_id, by_period, marked_as } = query;
+
+    const withTodoList = (queryBuilder, todo_list_id) => {
+        if(todo_list_id) {
+            queryBuilder.andWhere('l.id', todo_list_id);
+        }
+    };
+
+    const withMarkedAs = (queryBuilder, marked_as) => {
+        if(marked_as === 'complete') {
+            queryBuilder.andWhere('i.complete', true);
+        }
+        else if(marked_as === 'deleted') {
+            queryBuilder.whereNotNull('i.deleted_at');
+        }
+    };
+
     return db('todo_items as i')
         .innerJoin('todo_lists as l', 'i.todo_list_id', 'l.id')
         .innerJoin('user_todo_lists as u', 'l.id', 'u.todo_list_id')
         .where('u.user_id', userId)
+        .modify(withTodoList, todo_list_id)
+        .modify(withMarkedAs, marked_as)
         .select('i.*');
-
-    // var withUserName = function(queryBuilder, foreignKey) {
-    //     queryBuilder.leftJoin('users', foreignKey, 'users.id').select('users.user_name');
-    //   };
-    //   knex.table('articles').select('title', 'body').modify(withUserName, 'articles_user.id').then(function(article) {
-    //     console.log(article.user_name);
-    //   });
 };
 
 const getById = id => {
