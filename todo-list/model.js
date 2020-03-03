d038f71d87c5d50e60755d430549bd4bb090831b
Nolan Picini <nolanpic@gmail.com>
2020-03-02 19:09:24 -0800

    Rough in todo list model

---
+++ todo-list/model.js
@@ -0,0 +1,33 @@
+const db = require('../data/db');
+
+const getByUser = userId => {
+
+};
+
+const getById = id => {
+
+};
+
+const create = (userId, list) => {
+
+};
+
+const update = (id, updates) => {
+
+};
+
+const remove = id => {
+
+};
+
+const addUserToList = (userId, id) => {
+    // *!* STRETCH Todo
+};
+
+module.exports = {
+    getByUser,
+    getById,
+    create,
+    update,
+    remove
+};
