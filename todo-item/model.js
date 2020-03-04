8bff6bed2f66c052ca5fa0868046c1c014612d3b
Nolan Picini <nolanpic@gmail.com>
2020-03-03 18:00:43 -0800

    Finish route for getting todos + delete route

--- todo-item/model.js
+++ todo-item/model.js
@@ -8,18 +8,36 @@ const getByList = todoListId => {
 const getByQuery = (userId, query) => {
     const { todo_list_id, by_period, marked_as } = query;
 
+    // gets todos by list id
     const withTodoList = (queryBuilder, todo_list_id) => {
         if(todo_list_id) {
             queryBuilder.andWhere('l.id', todo_list_id);
         }
     };
 
+    // gets todos by period (for the day, for the month)
+    const withByPeriod = (queryBuilder, by_period) => {
+        if(by_period === 'month') {
+            queryBuilder.andWhere(db.raw("i.complete_by <= CURRENT_DATE + INTERVAL '30 day'"));
+        }
+        else if(by_period === 'day') {
+            queryBuilder.andWhere(db.raw("i.complete_by <= CURRENT_DATE + INTERVAL '1 day'"));
+        }
+    }
+
+    // gets todos that are marked as complete or deleted,
+    // also ensures that deleted todos don't show up otherwise
     const withMarkedAs = (queryBuilder, marked_as) => {
         if(marked_as === 'complete') {
-            queryBuilder.andWhere('i.complete', true);
+            queryBuilder
+                .andWhere('i.complete', true)
+                .andWhere('i.deleted_at', null);
         }
         else if(marked_as === 'deleted') {
             queryBuilder.whereNotNull('i.deleted_at');
+        }
+        else {
+            queryBuilder.andWhere('i.deleted_at', null);
         }
     };
 
@@ -29,6 +47,7 @@ const getByQuery = (userId, query) => {
         .where('u.user_id', userId)
         .modify(withTodoList, todo_list_id)
         .modify(withMarkedAs, marked_as)
+        .modify(withByPeriod, by_period)
         .select('i.*');
 };
 
@@ -54,7 +73,7 @@ const update = async (id, updates) => {
 const remove = id => {
     return db('todo_items')
         .where({ id })
-        .delete();
+        .update('deleted_at', db.raw('CURRENT_DATE'));
 };
 
 module.exports = {
--- todo-item/router.js
+++ todo-item/router.js
@@ -15,4 +15,31 @@ router.get('/', async (req, res) => {
     
 });
 
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const updates = req.body;
+    // *!* TODO - Ensure this todo belongs to this user
+    try {
+        const updatedTodo = await TodoItems.update(id, updates);
+        res.status(200).json(updatedTodo);
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json({ error: 'Something went wrong updating this todo item' });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    // *!* TODO - Ensure this todo belongs to this user
+    try {
+        await TodoItems.remove(id);
+        res.status(204).send();
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json({ error: 'Something went wrong deleting this todo item' });
+    }
+});
+
 module.exports = router;
